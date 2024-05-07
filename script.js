913192ba1ec04bf5079736aa5a06211a8c1a7822
Salvatoreing01 <salvatoreing01@gmail.com>
2024-05-07 21:05:28 +0200

    es 2

---
+++ script.js
@@ -0,0 +1,182 @@
+// let num1 = 5;
+// let num2 = 6;
+
+  
+
+
+    // es 1
+
+// console.log("il valore della varibile num1 e ", num1 );
+// console.log("il valore della varibile num2 e ", num2 );
+
+
+// let stringa = " ciao a tutti";
+
+// console.log("il valore della varibile stringa e ", stringa );
+
+
+// const PIGRECO = 3.14159265359;
+
+// console.log("il valore della varibile PIGRECO e ", PIGRECO );
+
+// PIGRECO = 2;
+
+
+
+// const numero = 10;
+// {
+// if ( numero > 0 )
+//     alert("il numero e positivo ");
+// } 
+// {
+// else if (numero < 0 ) 
+// } 
+// {
+// else
+// alert( "il numero e zero");
+// }
+
+
+
+// es prova  if e else 
+
+// let numero = prompt("scrivi un numero");
+
+// if (numero > 0) {
+//   alert("Il numero è positivo");
+// } else if (numero < 0) {
+//   alert("Il numero è negativo");
+// } else {
+//   alert("Il numero è zero");
+// }
+
+
+
+
+
+
+
+// es 2 data 07/05/2024
+
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let num4 = 4;
+let num5 = 5;
+
+
+// console.log( num1 + num2 + num3 + num4 + num5 );
+
+let totalesomma = num1 + num2 + num3 + num4 + num5;
+
+let totalemedia = totalesomma /5;
+
+let fsomma = "La somma tra i numeri equivale a";
+
+let fmedia = " e la media equivale a";
+
+console.log( fsomma, totalesomma , fmedia, totalemedia  );
+
+// console.log( `La somma tra i numeri equivale a ${totalesomma} e la media equivale a ${totalemedia}`);
+
+
+
+// let annocorrente = 2024;
+
+// let annosalvo = 2001;
+
+// let totaleeta = annocorrente - annosalvo;
+
+// // console.log(totaleeta);
+
+// let necessari = 100 - totaleeta;
+
+// console.log( `hai tot anni ${totaleeta} e ti mancano tot ${necessari} per compierne 100`);
+
+
+
+// let utente = (+prompt ("inserisci un nummero"));
+
+// let utente2 = (+prompt ("inserisci un altro nummero")); 
+
+// let sommautente = utente + utente2;
+
+// let sottrazioneutente = utente - utente2;
+
+// let moltiplicazioneutente = utente * utente2;
+
+// let divisioneutente = utente / utente2;
+
+// let potenzautente = utente ** utente2;
+
+// console.log( `Con i numeri da te scelti, i risultati delle varie operazioni sono: somma ${sommautente} sottazione ${sottrazioneutente} moltiplicazione ${ moltiplicazioneutente} divisone ${divisioneutente} potenza ${ potenzautente}`);
+
+
+
+
+let array_1 = [
+
+  [' un', ' per', ' incatenarli.'],
+
+  [' Anello', ' trovarli,'],
+
+  [' ghermirli', ' e'],
+
+  [' gondor', ' mark'],
+
+];
+
+let array_2 = [
+
+  [[' trovarli,']],
+
+  [' tu,', ' sciocchi'],
+
+  [' tu,', ' sciocchi', [' padron', ' Sauron']],
+
+  [' nel', [' fuggite', ' gandalf']],
+
+  [[' domarli,','passare'], ' buio']
+
+];
+
+// Un Anello per domarli, un Anello per trovarli, un Anello per ghermirli e nel buio incatenarli
+  
+console.log(array_1[0][0] + array_1[1][0] + array_1[0][1] + array_2[4][0][0] + array_1[0][0] + array_1[1][0] + array_1[0][1] );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
